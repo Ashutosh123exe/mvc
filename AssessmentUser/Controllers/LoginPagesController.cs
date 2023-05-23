7b9f57e08b9932fdbf9c7c9cae2d7944280f192a
Ashutosh123exe <Ashutosh@svam.com>
2023-05-23 13:37:45 +0530

    w

---
+++ AssessmentUser/Controllers/LoginPagesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AssessmentUser.Context;
+using AssessmentUser.Models;
+
+namespace AssessmentUser.Controllers
+{
+    public class LoginPagesController : Controller
+    {
+        private readonly DContext _context;
+
+        public LoginPagesController(DContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        
+        public IActionResult Login(LoginPage model)
+        {
+
+            
+            
+            if (model.Username == "admin" && model.Password == "password")
+                {
+                    
+                    return RedirectToAction("Index", "Home"); 
+                }
+                else
+                {
+                    
+                    ModelState.AddModelError("", "Invalid username or password");
+                }
+            
+
+            
+            return View(model);
+        }
+    }
+}
